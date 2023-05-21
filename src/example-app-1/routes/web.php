03512cb8b5015c494ae1a90ae036fcedad53cbc8
S HYS <34961313+s-hys31@users.noreply.github.com>
2023-05-21 20:55:49 +0900

    Analysis workflow test

--- src/example-app-1/routes/web.php
+++ src/example-app-1/routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/analysis-workflow', function () {
+    return view('welcome');
+});
